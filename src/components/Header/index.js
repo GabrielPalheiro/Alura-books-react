7ba74748f980130140985eb3e2eab5d2e7a2d571
GabrielPalheiro <gabriel_palheiro@hotmail.com>
2023-02-22 21:02:04 -0300

    Refact: refatorando componentes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,20 @@
 import Logo from "../Logo/index.js";
 import OpcoesHeader from "../OpcoesHeader/index.js";
 import IconesHeader from "../IconesHeader/index.js";
-import "./style.css";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  background-color: #eee9da;
+  display: flex;
+  justify-content: space-around;
+`;
 
 export default function Header() {
   return (
-    <header className="App-header">
+    <HeaderContainer>
       <Logo />
       <OpcoesHeader />
       <IconesHeader />
-    </header>
+    </HeaderContainer>
   );
 }
